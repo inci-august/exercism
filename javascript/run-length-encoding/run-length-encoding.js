3e340fee649f7e6099f3b4221d57e8925ace79c5
Inci <incissocial@gmail.com>
2018-12-09 17:47:33 +0400

    Simplify run-length-encoding solution

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,35 +1,8 @@
-var RLE = function() {};
-
-RLE.prototype.encode = function(input) {
-  let count = 1;
-  let output = '';
-
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] === input[i + 1]) {
-      count += 1;
-    } else {
-      count > 1 ? (output += count + input[i]) : (output += input[i]);
-      count = 1;
-    }
-  }
-
-  return output;
+const RLE = {
+  encode: input =>
+    input.replace(/(\w)\1+/g, (match, char) => `${match.length}${char}`),
+  decode: input =>
+    input.replace(/(\d+)(\w)/g, (match, digit, char) => char.repeat(digit))
 };
 
-RLE.prototype.decode = function(input) {
-  let numStr = '';
-  let output = '';
-
-  for (let i = 0; i < input.length; i++) {
-    if (+input[i]) {
-      numStr += input[i];
-    } else {
-      numStr ? (output += input[i].repeat(+numStr)) : (output += input[i]);
-      numStr = '';
-    }
-  }
-
-  return output;
-};
-
-module.exports = new RLE();
+module.exports = RLE;
